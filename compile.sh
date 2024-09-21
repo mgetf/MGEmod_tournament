18f8ef2adb780045468e65627fff203baf16f267
tommy-mor <thmorriss@gmail.com>
2024-09-21 16:14:14 -0400

    deleted old files

--- compile.sh
+++
@@ -1,2 +0,0 @@
-docker cp addons/sourcemod/scripting/mgeme.sp ff3:/home/tf2/hlserver/tf2/tf/addons/sourcemod/scripting
-docker exec -u 0 -it ff3 tf2/tf/addons/sourcemod/scripting/compile.sh
--- connect.el
+++
@@ -1,3 +0,0 @@
-(defun connect-remote ()
-  (interactive)
-  (dired "/ssh:root@104.248.8.232:/root/mgeme_sm/addons/sourcemod/scripting/"))
