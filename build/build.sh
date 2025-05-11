cb4bbca66fefa797fa56de390602808bafb927b0
tommy-mor <thmorriss@gmail.com>
2025-05-11 11:29:29 -0400

    build script works!!!

--- build/build.sh
+++ build/build.sh
@@ -7,3 +7,10 @@ docker run --rm \
   -i /opt/addons/sourcemod/scripting/include/ \
   /src/addons/sourcemod/scripting/ladder.sp \
   -o /src/addons/sourcemod/plugins/ladder.smx
+
+scp ../addons/sourcemod/plugins/ladder.smx  root@108.61.178.103:/tmp/
+ssh root@108.61.178.103 '
+  docker cp /tmp/ladder.smx tf2_ladder_service:/home/tf2/server/tf/addons/sourcemod/plugins &&
+  docker exec tf2_ladder_service ./rcon -H 127.0.0.1 -p 27015 -P 123456 sm plugins unload ladder &&
+  docker exec tf2_ladder_service ./rcon -H 127.0.0.1 -p 27015 -P 123456 sm plugins load ladder
+'
